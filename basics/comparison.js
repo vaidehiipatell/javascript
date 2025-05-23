708f03df4a36454a577571cfdeea2d78b08ffa80
vaidehiipatell <151128312+vaidehiipatell@users.noreply.github.com>
2025-05-23 22:52:52 +0000

    datatype

---
+++ basics/comparison.js
@@ -0,0 +1,18 @@
+console.log(2>1);
+console.log(2==1);
+console.log(2<1);
+
+//give the true // this convert the string in number then compare
+// so always compare the value with the data type
+console.log("2">1);
+console.log("02">1);
+
+//
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+
+//same case with the undefined -> false
+
+// === check the value and datatype
+console.log("2"===2);
---
+++ basics/datatye.js
@@ -0,0 +1,22 @@
+// premetive - call by value
+// -> string ,number,boolean , null ,undefined , symobole , bigInt
+
+// js is the dynamically type 
+
+//non premetive 
+// -> reference type
+// array , objects , function 
+
+const name = ["vaidehi" , "jenil" , "jay"];
+
+const obj = {
+    name : "vaidehi",
+    age : 19,
+    isTeen : false
+};
+
+const myfunction = function(){
+    console.log("heyy guys");
+};
+
+console.log(myfunction());
