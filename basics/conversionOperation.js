a2f263c234d5ecd1d8098f93440eee2484fa8f1a
vaidehiipatell <151128312+vaidehiipatell@users.noreply.github.com>
2025-05-23 14:06:04 +0000

    varible , constant , let and conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,9 @@
+// conversion 
+
+// byusing Number , string , Boolean 
+
+// boolean 
+// 1 = true
+//0 = false
+// string = true
+
---
+++ basics/let_const_var.js
@@ -0,0 +1,12 @@
+// prefer to not use var 
+// because of the block scope and function scope
+const accountId = "12345";
+let userId = "vaidehiipatell";
+var password = "123qwe";
+gmail = "vaidehipatel@gmail.com";
+let account; // undefined 
+
+console.log(userId);
+console.log(accountId);
+
+console.table([accountId,userId,password,gmail,account]);
